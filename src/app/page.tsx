7103c82255d629938f72b0194dcb2f5e48df87c3
linarevicheva <126851225+linarevicheva@users.noreply.github.com>
2024-09-17 18:27:25 -0700

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,143 @@
-import Image from "next/image";
+"use client";
+
+import React from 'react';
+
+import { Heading, Text, Flex, Button, Grid, Icon, Logo, Background, LetterFx } from '@/once-ui/components';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const links = [
+		{
+			href: "https://",
+			title: "Monitor",
+			description: "Go to main dashboard.",
+		},
+		{
+			href: "https://",
+			title: "History",
+			description: "Analyze past events.",
+		},
+		{
+			href: "https://",
+			title: "Stay tuned",
+			description: "Set up notifications.",
+		},
+	];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  const handleEmail = () => {
+    window.location.href = "mailto:linarevicheva@gmail.com"
+  }
+
+	return (
+		<Flex
+			fillWidth paddingTop="l" paddingX="l"
+			direction="column" alignItems="center" flex={1}>
+			<Background
+				dots={false}/>
+			<Flex
+				position="relative"
+				as="section" overflow="hidden"
+				fillWidth minHeight="0" maxWidth={68}
+				direction="column" alignItems="center" flex={1}>
+				<Flex
+					as="main"
+					direction="column" justifyContent="center"
+					fillWidth fillHeight padding="l" gap="l">
+					<Flex
+						mobileDirection="column"
+						fillWidth gap="24">
+						<Flex
+							position="relative"
+							flex={2} paddingTop="56" paddingX="xl">
+							<Logo size="xl" icon={false} style={{zIndex: '1'}}/>
+						</Flex>
+						<Flex
+							position="relative"
+							flex={4} gap="24" marginBottom="104"
+							direction="column">
+                <Heading
+								wrap="balance"
+								variant="display-strong-xl">
+								<span className="font-code">
+									<LetterFx
+										trigger="instant">
+										Salem
+									</LetterFx>
+								</span>
+							</Heading>
+							<Heading
+								wrap="balance"
+								variant="display-strong-s">
+								<span className="font-code">
+									<LetterFx
+										trigger="instant">
+										We hunt before you know
+									</LetterFx>
+								</span>
+							</Heading>
+							<Button
+								href="https://once-ui.com/docs"
+								suffixIcon="chevronRight"
+								variant="secondary">
+								Start now
+							</Button>
+						</Flex>
+					</Flex>
+					<Grid
+						radius="l"
+						border="neutral-medium"
+						borderStyle="solid-1"
+						columns="repeat(3, 1fr)"
+						tabletColumns="1col"
+						mobileColumns="1col"
+						fillWidth>
+						{links.map((link) => (
+							<Link
+								target="_blank"
+								style={{ padding: 'var(--responsive-space-l)' }}
+								key={link.href}
+								href={link.href}>
+								<Flex
+									fillWidth paddingY="8" gap="8"
+									direction="column">
+									<Flex
+										fillWidth gap="12"
+										alignItems="center">
+										<Text
+											variant="body-strong-m" onBackground="neutral-strong">
+											{link.title}
+										</Text>
+										<Icon size="s" name="arrowUpRight" />
+									</Flex>
+									<Text
+										variant="body-default-s" onBackground="neutral-weak">
+										{link.description}
+									</Text>
+								</Flex>
+							</Link>
+						))}
+					</Grid>
+				</Flex>
+			</Flex>
+			<Flex
+				as="footer"
+				position="relative"
+				fillWidth paddingX="l" paddingY="m"
+				justifyContent="space-between">
+				<Flex
+					gap="12">
+					<Button
+            onClick={handleEmail}
+						prefixIcon="paperPlane" size="s" variant="tertiary">
+						Contact us (please don't)
+					</Button>
+          <Button
+						href="https://github.com/"
+						prefixIcon="github" size="s" variant="tertiary">
+						See code
+					</Button>
+				</Flex>
+			</Flex>
+		</Flex>
+	);
 }
